76f49e7509e765845fc619fe55b254ca699edfd1
Stegano <stegano@naver.com>
2017-08-12 14:45:21 +0900

    웹팩 설정 추가, 번들링 결과물 추가

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+/**
+ * 스타일은 `webpack` 번들링 과정에서 html 문서에 스크립트 파일과 함께 추가 됩니다!
+ * */
+require("./index.css");
+
 function startApp() {
   require("./component/Display");
   require("./component/Payment");
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.min.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html'
+    }),
+    new ExtractTextPlugin("index.min.css")
+  ]
+};
