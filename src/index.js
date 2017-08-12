b0781dd2b19946dbed526271dae43c854615902c
Stegano <stegano@naver.com>
2017-08-12 12:59:26 +0900

    앱 실행시점 변경

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
-require("./component/Display");
-require("./component/Payment");
-require("./component/Console");
+function startApp() {
+  require("./component/Display");
+  require("./component/Payment");
+  require("./component/Console");
+}
 
+if (document && "DOMContentLoaded" in document) {
+  document.addEventListener("DOMContentLoaded", function () {
+    startApp();
+  });
+} else {
+  /**
+   * MSIE8 대응
+   * */
+  document.onreadystatechange = function () {
+    if (document.readyState === "complete") {
+      startApp();
+    }
+  }
+}
