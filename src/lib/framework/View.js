ae7fc91595de8e79b91258436211eff42206ba74
Stegano <stegano@naver.com>
2017-08-13 18:08:47 +0900

    `View.js` 누락된 주석 추가

--- src/lib/framework/View.js
+++ src/lib/framework/View.js
@@ -3,18 +3,39 @@ var utils = require("../utils");
 /**
  * 컴포넌트를 구조적으로 사용하고 관리하기 위한 뷰 객체
  * @constructor
+ * @example
+ *
+ * // 뷰 객체 구현(확장)
+ * var Component = View.extend({
+ *   // `View.execute` 함수가 실행되면 최초로 실행되는 함수
+ *   initialize: function() { ... },
+ *   // 그외 원하는대로 정의하여 사용!
+ *   render: function() { ... },
+ *   values: { a: 1, b: 2, c: 3, ... }
+ * });
+ *
+ * // 구현된 뷰 객체 실행
+ * Component.exectue({
+ *   title : "View Component",
+ *   defaultValue: "1234"
+ *   ...
+ * });
  * */
 var View = function (options) {
   var _options = options || {};
   /**
-   * 현재 구현된 뷰를 확장하여 리턴하는 객체
+   * 현재 구현된 뷰를 확장하여 반환하는 객체
+   * @param {Object} extOptions 뷰 구현 정보
+   * @return {Object} View
    * */
   this.extend = function (extOptions) {
-    var _extOptions = utils.extend(extOptions, _options);
+    var _extOptions = utils.extend(extOptions, _options) || {};
     return View.prototype.extend(_extOptions);
   };
   /**
    * 뷰 컴포넌트 실행 함수
+   * @param {Object} [properties] `initialize`함수에 전달될 값
+   * @return {Object} options
    * */
   this.execute = function (properties) {
     if ("initialize" in _options && typeof _options.initialize === "function") {
@@ -23,9 +44,10 @@ var View = function (options) {
     return _options;
   };
 };
-
 /**
- * 현재 구현된 뷰를 확장하여 리턴하는 객체
+ * 뷰를 확장하여 반환하는 객체
+ * @param {Object} extOptions 뷰 구현 정보
+ * @return {Object} View
  * */
 View.prototype.extend = function (extOptions) {
   var _extOptions = extOptions || {};
